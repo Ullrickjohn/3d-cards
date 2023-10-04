32c95fbadc7e723b90776953d77617c7c6d39941
John <ullrickjohn@gmail.com>
2023-10-04 13:44:38 +0500

    feat complated 3d animation to card in JavasCript

--- 3d-card/script.js
+++ 3d-card/script.js
@@ -12,14 +12,30 @@ cardWrapper.addEventListener('mousemove', event =>{
 const x = (window.innerWidth / 2) - event.x
 const y = (window.innerHeight / 2) - event.y
 
-card.style.transform = `rotateX(${y / 35}deg) rotateY(${x / 25}deg)`
+card.style.transform = `rotateX(${y / -35}deg) rotateY(${x / 25}deg)`
+card.style.transition = `0s`
 
 image.style.transform = `translateZ(70px) rotate(${x / 20}deg)`
 circle.style.transform = `translateZ(-50px) rotate(${x }deg) scale(0.8) `
 title.style.transform = `translateZ(40px)  `
-text.style.transform = `translateZ(30px)  `
-buttonsBlock.style.transform = `translateZ(40px)  `
-mainBtn.style.transform = `translateZ(70px) rotate(${x / 20}deg)  `
+text.style.transform = `translateZ(20px)  `
+buttonsBlock.style.transform = `translateZ(30px)  `
+mainBtn.style.transform = `translateZ(50px)   `
+
+
+
+})
+cardWrapper.addEventListener('mouseleave', () =>{
+
+card.style.transform = `rotateX(0deg) rotateY(0deg)`
+card.style.transition = `0.7s`
+
+image.style.transform = `translateZ(0) `
+circle.style.transform = `translateZ(0)  `
+title.style.transform = `translateZ(0)  `
+text.style.transform = `translateZ(0)  `
+buttonsBlock.style.transform = `translateZ(0)  `
+mainBtn.style.transform = `translateZ(0)   `
 
 
 
